435d89ca645b6e6ef6dc354702d4ddd6f9bc46b7
Ashwin Nayak <anayak@mpi-magdeburg.mpg.de>
2024-11-09 02:26:48 +0100

    add entrypoint scripts

---
+++ 2D-room-example/main.jl
@@ -0,0 +1,5 @@
+include("convergence_plot_from_fem_order1.jl")
+include("convergence_plot_from_fem_order2.jl")
+include("plot_fem_order1.jl")
+include("plot_fem_order2.jl")
+include("plot_pressure_field_order1.jl")
---
+++ 2D-square-noise-example/main.jl
@@ -0,0 +1,5 @@
+include("convergence_plot_from_noise_order1.jl")
+include("convergence_plot_from_noise_order2.jl")
+include("plot_noise_order1.jl")
+include("plot_noise_order2.jl")
+include("plot_solutions_with_noise_order1.jl")
